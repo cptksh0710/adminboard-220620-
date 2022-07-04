a96ac4840791e74d786c92f03aab1695e481c3cf
cptksh0710 <cptksh0710@gmail.com>
2022-07-04 15:58:40 +0900

    세션 로그인, 로그아웃, 회원정보 리스트 수정

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-
+const db = require('../../app/models/db.js');
 const ctrl = require("./home.ctrl");
 
 // 페이지 라우팅 설정
@@ -10,12 +10,56 @@ router.get("/", ctrl.start);
 router.get("/register", ctrl.register);
 router.get("/dashboard", ctrl.dashboard);
 router.get("/profile", ctrl.profile);
-router.get("/userlist", ctrl.userlist);
+router.get("/userlist", function(req,res,next){
+  var sql = "SELECT no, name, id, password, permission, date_format(reg_date,'%Y-%m-%d %H:%i:%s') reg_date, "+
+            " date_format(mod_date,'%Y-%m-%d %H:%i:%s') mod_date from user;";
+  db.query(sql, function (err, data, fields) {
+  if (err) throw err;
+  res.render('home/userlist', { title: 'User List', userData: data});
+  });
+});
 router.get("/user_reg", ctrl.userreg);
 router.get("/import", ctrl.dataimport);
 router.get("/code", ctrl.code);
 router.get("/code_reg", ctrl.codereg);
 router.get("/export", ctrl.dataexport);
 router.get("/datalist", ctrl.datalist);
+
+// 로그아웃
+/*
+router.get('/logout', function (request, response) { 
+  request.session.destroy(function(err){ 
+    response.clearCookie("sid");
+    response.redirect('/'); 
+  });
+ }); */
+
+/*
+router.get('/logout', function (req, res) { 
+  req.session.destroy(() => { 
+    res.clearCookie("sid");
+    res.redirect('/'); 
+  });
+});*/
+
+
+router.get("/logout", async function (req, res, next) {
+  var session = req.session;
+  try {
+    if (session.user) {
+      //세션정보가 존재하는 경우
+      await req.session.destroy(function (err) {
+        if (err) console.log(err);
+        else {
+          res.redirect("/");
+        }
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  res.clearCookie("sid");
+  res.redirect("/");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const session = require('express-session');
 const bodyParser = require("body-parser");
 
 const app = express();
+const connection = require("./app/models/db")
 const home = require("./routes/home");
 
 app.use("/", home);
@@ -10,6 +12,14 @@ app.use(express.static("app"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//로그인 세션 미들웨어 설정
+app.use(session({
+  key: 'sid',
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
@@ -17,12 +27,33 @@ app.get("/", function(request,response){
   response.render("home/index");
 });
 
-app.get("/login", function(request,response){
-  response.render("home/login");
-});
-
-app.get("/login", function(request,response){
-  response.render("home/dashboard");
+//로그인 인증
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let userid = request.body.user_id;
+	let password = request.body.password;
+	// id, 비밀번호 입력을 모두 입력한 상태라면
+	if (userid && password) {
+		// SQL 쿼리문을 통해 입력한 로그인 유저 정보와 DB 유저 정보 일치 여부 확인
+		connection.query('SELECT * FROM user WHERE id = ? AND password = ?', [userid, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// ID가 존재한다면
+			if (results.length > 0) {
+				// 인증된 유저
+				request.session.loggedin = true;
+				request.session.userid = userid;
+				// 메인화면으로 Redirect
+				response.redirect('/dashboard');
+			} else {
+				response.send('ID 또는 비밀번호가 일치하지 않습니다.');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('ID와 비밀번호를 입력해 주세요.');
+		response.end();
+	}
 });
 
 require("./app/routes/board.routes.js")(app);
